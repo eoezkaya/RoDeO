9aa0db6aa41a7dc67428a1b6ffcfd9a47586824e
emre <emre.oezkaya@scicomp.uni-kl.de>
2021-02-26 14:28:26 +0100

    added some unit tests for aux and matrix-vector ops

---
+++ src/matrix_vector_operations_test.cpp
@@ -0,0 +1,67 @@
+/*
+ * RoDeO, a Robust Design Optimization Package
+ *
+ * Copyright (C) 2015-2020 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+ * Homepage: http://www.scicomp.uni-kl.de
+ * Contact:  Prof. Nicolas R. Gauger (nicolas.gauger@scicomp.uni-kl.de) or Dr. Emre Özkaya (emre.oezkaya@scicomp.uni-kl.de)
+ *
+ * Lead developer: Emre Özkaya (SciComp, TU Kaiserslautern)
+ *
+ * This file is part of RoDeO
+ *
+ * RoDeO is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * RoDeO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU
+ * General Public License along with CoDiPack.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Emre Özkaya, (SciComp, TU Kaiserslautern)
+ *
+ *
+ *
+ */
+
+#include "matrix_vector_operations.hpp"
+#include<gtest/gtest.h>
+
+
+TEST(testMatrixVectorOperations, testcopyRowVector){
+
+	rowvec a(10,fill::randu);
+	rowvec b(5, fill::randu);
+
+	copyRowVector(a,b);
+
+	EXPECT_EQ(a(0), b(0));
+	EXPECT_EQ(a(1), b(1));
+	EXPECT_EQ(a(2), b(2));
+	EXPECT_EQ(a(3), b(3));
+	EXPECT_EQ(a(4), b(4));
+
+}
+
+
+TEST(testMatrixVectorOperations, testnormalizeRowVector){
+
+	rowvec x(5,fill::randu);
+	vec xmin(5); xmin.fill(0.1);
+	vec xmax(5); xmax.fill(2.1);
+
+	rowvec xNormalized = normalizeRowVector(x, xmin, xmax);
+	double xCheck = xNormalized(0)*5 * (2.0) + 0.1;
+
+	double error = fabs(xCheck-x(0));
+	EXPECT_LT(error, 10E-10);
+
+}
+
+
+
---
+++ src/test_auxiliary_functions.cpp
@@ -0,0 +1,83 @@
+/*
+ * RoDeO, a Robust Design Optimization Package
+ *
+ * Copyright (C) 2015-2020 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+ * Homepage: http://www.scicomp.uni-kl.de
+ * Contact:  Prof. Nicolas R. Gauger (nicolas.gauger@scicomp.uni-kl.de) or Dr. Emre Özkaya (emre.oezkaya@scicomp.uni-kl.de)
+ *
+ * Lead developer: Emre Özkaya (SciComp, TU Kaiserslautern)
+ *
+ * This file is part of RoDeO
+ *
+ * RoDeO is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * RoDeO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU
+ * General Public License along with CoDiPack.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Emre Özkaya, (SciComp, TU Kaiserslautern)
+ *
+ *
+ *
+ */
+
+#include "auxiliary_functions.hpp"
+#include<gtest/gtest.h>
+
+TEST(testAuxiliaryFunctions, testifGoogleTestWorks){
+
+
+}
+
+
+TEST(testAuxiliaryFunctions, testcalculatePolynomial){
+
+	double x = 1.2;
+	rowvec coeffs(3);
+	coeffs(0) = 1.0;
+	coeffs(1) = 2.0;
+	coeffs(2) = 4.0;
+
+	double result = calculatePolynomial(x, coeffs);
+	EXPECT_EQ(result, 9.16);
+
+
+}
+
+TEST(testAuxiliaryFunctions, solveLinearSystemCholesky){
+
+
+	unsigned int dim = 10;
+
+	mat M(dim,dim,fill::randu);
+	mat A = M*trans(M);
+
+	mat U = chol(A);
+
+	vec b(dim,fill::randu);
+	vec x(dim,fill::randu);
+
+	solveLinearSystemCholesky(U, x, b);
+
+	vec Xexact = inv(A)*b;
+
+	for(unsigned int i=0; i<dim; i++){
+
+		double err = fabs(x(i) - Xexact(i));
+		EXPECT_LE(err, 10E-6);
+
+	}
+
+
+
+
+}
+
