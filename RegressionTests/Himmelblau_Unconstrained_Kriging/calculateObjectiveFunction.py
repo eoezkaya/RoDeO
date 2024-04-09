b4d98878785df079a9c85411c725394ac7950a3a
eoezkaya <emre.oezkaya@scicomp.uni-kl.de>
2024-04-09 12:47:36 +0200

    fixed small issue in Himmelblau testcase

--- RegressionTests/Himmelblau_Unconstrained_Kriging/calculateObjectiveFunction.py
+++ RegressionTests/Himmelblau_Unconstrained_Kriging/calculateObjectiveFunction.py
@@ -3,9 +3,7 @@
 import numpy as np 
 from himmelblau_optimization import HimmelblauOptimization
 himmelblauOpt = HimmelblauOptimization()
-def Himmelblau(x):
-    return (((x[0]**2+x[1]-11)**2) + (((x[0]+x[1]**2-7)**2)))
-    
+
     
 
 
