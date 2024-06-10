e97af06d4d3cadfd682dac442fa5f5fbcec9d060
eoezkaya <emre.oezkaya@scicomp.uni-kl.de>
2024-06-10 13:07:38 +0200

    remove GUI

--- src/GUI/parameters.py
+++
@@ -1,9 +0,0 @@
-class SingleParameter:
-    def __init__(self,name, type, lb, ub, increment = 0):
-        self.name = name
-        self.lb =lb
-        self.ub =ub
-        self.type = type
-        self.increment = increment
-    def print(self):
-        print(self.name, self.type, self.lb, self.ub, self.increment)    
