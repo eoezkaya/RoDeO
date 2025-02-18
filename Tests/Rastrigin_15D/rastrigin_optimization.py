d522e006702eae7ffd6b7b5ed29bb71ca6805b2c
eoezkaya <emre.oezkaya@scicomp.uni-kl.de>
2025-02-18 12:58:09 +0100

    removed test cases for executable

--- Tests/Rastrigin_15D/rastrigin_optimization.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-class RastriginOptimization:
-   
-    def evaluateFunction(self, x):
-        sum = 0.0;
-        for i in range(15):
-            sum = sum + x[i]**2 - 10.0*np.cos(2*3.141592*x[i])
-        val = 10*15 + sum
-        return val
