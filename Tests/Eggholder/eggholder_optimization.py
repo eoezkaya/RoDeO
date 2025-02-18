5582bcb1fe85a5c4e6c54b724a085cd5a4778523
eoezkaya <emre.oezkaya@scicomp.uni-kl.de>
2025-02-18 12:57:13 +0100

    removed test cases for executable

--- Tests/Eggholder/eggholder_optimization.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-class EggholderOptimization:
-   
-    def evaluateFunction(self, x):
-        return -(x[1]+47.0)*np.sin(np.sqrt(abs(x[1]+0.5*x[0]+47.0)))-x[0]*np.sin(np.sqrt(abs(x[0]-(x[1]+47.0) )))	        
-    
-
-
